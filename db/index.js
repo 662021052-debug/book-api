9d3ffb964cdb03d27228c81cf74ed9168b207dba
662021052 <662021052@tsu.ac.th>
2026-02-05 16:53:13 +0700

    Lab9_API_052

---
+++ db/index.js
@@ -0,0 +1,12 @@
+// const {Pool} = require('pg');
+import { Pool } from 'pg'; 
+
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config()
+
+const popl = new pool({
+  connectionString: process.env.DATABASE_URL,
+});
+// module.exports = pool;
+export default pool;
